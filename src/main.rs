4fec7cc6bf7e6045e2cea62ef0b9077e8d9ae81a
allivka <allivka1920@gmail.com>
2026-06-23 03:06:10 +0300

    fixed bu and added new functionality to cnavas struct

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use minifb::{Key, Window, WindowOptions};
+use minifb::{Key, Scale, ScaleMode, Window, WindowOptions};
 use std::cmp::{min, max};
 const WIDTH: usize = 640;
 const HEIGHT: usize = 360;
 
-struct Color(u8, u8, u8, u8);
+#[derive(Copy, Clone)]
+struct RGBA(u8, u8, u8, u8);
 
-impl Color {
-    fn tou32(self) -> u32 {
-        (self.0 as u32) << 16 | (self.1 as u32) << 8 | (self.2 as u32)
+impl RGBA {
+    fn tou32(&self) -> u32 {
+        (self.3 as u32) << 24 | (self.0 as u32) << 16 | (self.1 as u32) << 8 | (self.2 as u32)
     }
 }
 
@@ -30,7 +31,7 @@ impl Canvas {
     }
 
 
-    fn fix(&self, mut pos: Vec2isize) -> Vec2isize {
+    fn fixed(&self, mut pos: Vec2isize) -> Vec2isize {
         pos.0 = min(pos.0, self.width);
         pos.0 = min(pos.1, self.height);
         pos.0 = max(pos.0, 0);
@@ -39,7 +40,7 @@ impl Canvas {
         pos
     }
 
-    fn fix_r(&self, pos: &mut Vec2isize) {
+    fn fix(&self, pos: &mut Vec2isize) {
         pos.0 = min(pos.0, self.width);
         pos.0 = min(pos.1, self.height);
         pos.0 = max(pos.0, 0);
@@ -47,52 +48,79 @@ impl Canvas {
 
     }
 
-    fn set(&mut self, pos: Vec2isize, color: Color) {
-        self.buffer[((pos.1 - 1) * self.width + pos.0) as usize] = color.tou32();
+    fn idx(pos: Vec2isize, canvas_size: Vec2isize) -> usize {
+        min((max(pos.1 - 1, 0) * canvas_size.0 + pos.0), canvas_size.0 * canvas_size.1 - 1) as usize
     }
 
-    fn set_fix(&mut self, mut pos: Vec2isize, color: Color) {
-        self.fix_r(&mut pos);
-        self.buffer[((pos.1 - 1) * self.width + pos.0) as usize] = color.tou32();
+    fn idxx(self, pos: Vec2isize) -> usize {
+        min((max(pos.1 - 1, 0) * self.width + pos.0), self.width * self.height - 1) as usize
     }
 
-    fn draw_line(&mut self, mut start: Vec2isize, mut end: Vec2isize, color: Color) {
-        self.fix_r(&mut start);
-        self.fix_r(&mut end);
+    fn set(&mut self, pos: Vec2isize, color: RGBA) {
+        self.buffer[Self::idx(pos, Vec2isize(self.width, self.height))] = color.tou32();
+    }
+
+    fn set_fixed(&mut self, mut pos: Vec2isize, color: RGBA) {
+        self.fix(&mut pos);
+        self.buffer[(max(pos.1 - 1, 0) * self.width + pos.0) as usize] = color.tou32();
+    }
+
+    fn fill(&mut self, mut start: Vec2isize, mut end: Vec2isize, color: RGBA) {
+        self.fix(&mut start);
+        self.fix(&mut end);
+
+        for y in min(start.1, end.1)..max(start.1, end.1) {
+            for x in min(start.0, end.0)..max(start.0, end.0) {
+                self.set(Vec2isize(x, y), color);
+            }
+        }
+    }
+
+    fn draw_line(&mut self, mut start: Vec2isize, mut end: Vec2isize, color: RGBA) {
+        self.fix(&mut start);
+        self.fix(&mut end);
 
         let d0 = end.0 - start.0;
         let d1 = end.1 - start.1;
         let k = d1 as f32 / d0 as f32 * d1.signum() as f32;
 
-        for i in start.0..end.0 {
+        for i in min(start.0, end.0)..=max(start.0, end.0) {
             self.set(Vec2isize(start.0 + i * d0.signum(), (start.1 as f32 + (i as f32 * k)) as isize), color);
         }
     }
 }
 
 fn main() {
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let mut canvas = Canvas::new(WIDTH, HEIGHT);
 
     let mut window = Window::new(
         "Test - ESC to exit",
         WIDTH,
         HEIGHT,
-        WindowOptions::default(),
+        WindowOptions {
+            borderless: false,
+            title: true,
+            resize: true,
+            scale: Scale::X1,
+            scale_mode: ScaleMode::Center,
+            topmost: true,
+            transparency: false,
+            none: false,
+
+        },
     )
-        .unwrap_or_else(|e| {
-            panic!("{}", e);
-        });
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
 
     window.set_target_fps(60);
 
+    canvas.fill(Vec2isize(0, 0), Vec2isize(200, 200), RGBA(0, 0, 255, 0));
+    canvas.draw_line(Vec2isize(0, 0), Vec2isize(200, 200), RGBA(255, 255, 255, 0));
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        for i in buffer.iter_mut() {
-            *i = 0; // write something more funny here!
-        }
-
         window
-            .update_with_buffer(&buffer, WIDTH, HEIGHT)
+            .update_with_buffer(&canvas.buffer, WIDTH, HEIGHT)
             .unwrap();
     }
 }
