767ceafed25497ce8fb071eee78355ccd2b6b044
allivka <allivka1920@gmail.com>
2026-06-24 16:30:18 +0300

    updated some stuff and added new functions to the api. Canvas::fill_triangle functions needs the fix

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,56 @@
 use minifb::{Key, Scale, ScaleMode, Window, WindowOptions};
 use std::cmp::{min, max};
-const WIDTH: usize = 640;
-const HEIGHT: usize = 360;
+
+const WIDTH: usize = 1920;
+const HEIGHT: usize = 1080;
+
+#[derive(Copy, Clone)]
+struct Vec2isize(isize, isize);
+
+#[derive(Copy, Clone)]
+struct LineCoefficient {
+    k: f32,
+    b: f32
+}
+
+impl LineCoefficient {
+    fn new_from_line(start: Vec2isize, end: Vec2isize) -> Self {
+        let dx = (end.0 - start.0) as f32;
+        let dy = (end.1 - start.1) as f32;
+
+        let k = if dx == 0.0 {
+            f32::INFINITY
+        } else {
+            dy / dx
+        };
+
+        let b = if dx == 0.0 {
+            start.0 as f32
+        } else {
+            start.0 as f32 - k * start.0 as f32
+        };
+
+        LineCoefficient { k, b }
+    }
+}
 
 #[derive(Copy, Clone)]
 struct RGBA(u8, u8, u8, u8);
 
 impl RGBA {
-    fn tou32(&self) -> u32 {
+
+    fn from_argb_u32(n: u32) -> RGBA {
+        RGBA(
+            (0b1111_1111 & (n >> 16)) as u8,
+            (0b1111_1111 & (n >> 8)) as u8,
+            (0b1111_1111 & (n >> 0)) as u8,
+            (0b1111_1111 & (n >> 24)) as u8
+        )
+    }
+    fn to_argb_u32(&self) -> u32 {
         (self.3 as u32) << 24 | (self.0 as u32) << 16 | (self.1 as u32) << 8 | (self.2 as u32)
     }
 }
-
-struct Vec2isize(isize, isize);
-
 
 struct Canvas {
     width: isize,
@@ -45,48 +82,97 @@ impl Canvas {
         pos.0 = min(pos.1, self.height);
         pos.0 = max(pos.0, 0);
         pos.0 = max(pos.1, 0);
+    }
 
+    fn check(&self, pos: Vec2isize) -> bool {
+        pos.0 >= 0 && pos.0 < self.width && pos.1 >= 0 && pos.1 < self.height
     }
 
     fn idx(pos: Vec2isize, canvas_size: Vec2isize) -> usize {
-        min((max(pos.1 - 1, 0) * canvas_size.0 + pos.0), canvas_size.0 * canvas_size.1 - 1) as usize
+        min(max(pos.1 - 1, 0) * canvas_size.0 + pos.0, canvas_size.0 * canvas_size.1 - 1) as usize
     }
 
-    fn idxx(self, pos: Vec2isize) -> usize {
-        min((max(pos.1 - 1, 0) * self.width + pos.0), self.width * self.height - 1) as usize
+    fn idx_of(&self, pos: Vec2isize) -> usize {
+        min(max(pos.1 - 1, 0) * self.width + pos.0, self.width * self.height - 1) as usize
     }
 
     fn set(&mut self, pos: Vec2isize, color: RGBA) {
-        self.buffer[Self::idx(pos, Vec2isize(self.width, self.height))] = color.tou32();
+        if !self.check(pos) { return; }
+        self.buffer[Self::idx(pos, Vec2isize(self.width, self.height))] = color.to_argb_u32();
+    }
+
+    fn get(&self, pos: Vec2isize) -> RGBA {
+        RGBA::from_argb_u32(self.buffer[self.idx_of(pos)])
     }
 
     fn set_fixed(&mut self, mut pos: Vec2isize, color: RGBA) {
         self.fix(&mut pos);
-        self.buffer[(max(pos.1 - 1, 0) * self.width + pos.0) as usize] = color.tou32();
-    }
-
-    fn fill(&mut self, mut start: Vec2isize, mut end: Vec2isize, color: RGBA) {
-        self.fix(&mut start);
-        self.fix(&mut end);
-
-        for y in min(start.1, end.1)..max(start.1, end.1) {
-            for x in min(start.0, end.0)..max(start.0, end.0) {
-                self.set(Vec2isize(x, y), color);
-            }
-        }
+        self.buffer[(max(pos.1 - 1, 0) * self.width + pos.0) as usize] = color.to_argb_u32();
     }
 
     fn draw_line(&mut self, mut start: Vec2isize, mut end: Vec2isize, color: RGBA) {
         self.fix(&mut start);
         self.fix(&mut end);
 
-        let d0 = end.0 - start.0;
-        let d1 = end.1 - start.1;
-        let k = d1 as f32 / d0 as f32 * d1.signum() as f32;
+        let line = LineCoefficient::new_from_line(start, end);
 
-        for i in min(start.0, end.0)..=max(start.0, end.0) {
-            self.set(Vec2isize(start.0 + i * d0.signum(), (start.1 as f32 + (i as f32 * k)) as isize), color);
+        if line.k.is_infinite() {
+            let y_start = min(start.1, end.1);
+            let y_end = max(start.1, end.1);
+            for y in y_start..=y_end {
+                self.set(Vec2isize(start.0, y), color);
+            }
+            return;
         }
+
+        let x_start = min(start.0, end.0);
+        let x_end = max(start.0, end.0);
+
+        for x in x_start..=x_end {
+            let y = (line.k * x as f32 + line.b) as isize;
+            self.set(Vec2isize(x, y), color);
+        }
+    }
+
+    fn fill_rect(&mut self, mut start: Vec2isize, mut end: Vec2isize, color: RGBA) {
+        self.fix(&mut start);
+        self.fix(&mut end);
+
+        for y in min(start.1, end.1)..=max(start.1, end.1) {
+            for x in min(start.0, end.0)..=max(start.0, end.0) {
+                self.set(Vec2isize(x, y), color);
+            }
+        }
+    }
+
+    fn fill_triangle(&mut self, mut lv: Vec2isize, mut mv: Vec2isize, mut hv: Vec2isize, color: RGBA) {
+        if mv.1 < lv.1 { std::mem::swap(&mut mv, &mut lv); }
+        if hv.1 < lv.1 { std::mem::swap(&mut hv, &mut lv); }
+        if hv.1 < mv.1 { std::mem::swap(&mut hv, &mut mv); }
+
+        let lh_coef = LineCoefficient::new_from_line(lv, hv);
+
+        fn fill_half(this: &mut Canvas, low: Vec2isize, high: Vec2isize, lh_coef: LineCoefficient, color: RGBA) {
+
+            let coef = LineCoefficient::new_from_line(high, low);
+            let mut y: isize;
+            let mut x_boundary: isize;
+
+            for x in min(low.0, high.0)..=max(low.0, high.0) {
+                y = (x as f32 * coef.k + coef.b) as isize;
+                x_boundary = ((y as f32 - lh_coef.b) / lh_coef.k) as isize;
+
+                for i in min(x, x_boundary)..=max(x, x_boundary) {
+                    this.set(Vec2isize(i, y), color);
+                }
+
+            }
+        }
+
+        fill_half(self, lv, mv, lh_coef, color);
+        fill_half(self, mv, hv, lh_coef, color);
+
+
     }
 }
 
@@ -115,8 +201,9 @@ fn main() {
 
     window.set_target_fps(60);
 
-    canvas.fill(Vec2isize(0, 0), Vec2isize(200, 200), RGBA(0, 0, 255, 0));
+    canvas.fill_rect(Vec2isize(0, 0), Vec2isize(200, 200), RGBA(0, 0, 255, 0));
     canvas.draw_line(Vec2isize(0, 0), Vec2isize(200, 200), RGBA(255, 255, 255, 0));
+    canvas.fill_triangle(Vec2isize(0, 300), Vec2isize(300, 300), Vec2isize(300, 450), RGBA(255, 0, 0, 0));
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         window
