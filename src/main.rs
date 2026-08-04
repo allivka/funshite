c2761759dd56746c5a2d1203b08a4b0c28d91c0c
allivka <allivka1920@gmail.com>
2026-08-04 15:32:56 +0300

    updated main.rs to use new code

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use std::path::Path;
 use base::{Vec2i, RGBA};
 use canvas::Canvas;
 use object::Object;
+use crate::base::Vec3d;
 
 const WIDTH: usize = 1920;
 const HEIGHT: usize = 1080;
@@ -42,6 +43,11 @@ fn main() {
 
     window.set_target_fps(60);
 
+    let mut config = settings::Settings::default_settings();
+
+    let mut camera = viewer::Viewer::new(Vec3d(0.0, 0.0, 0.0));
+
+
     let objects = match object::parse_file(Path::new("test.obj")) {
         Ok(objects) => {
             println!("Successfully parsed obj file");
@@ -52,6 +58,8 @@ fn main() {
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
 
+        controller::process_controls(&mut config, &mut camera, &window.get_keys());
+
         canvas.fill_rect(Vec2i(0, 0), Vec2i(200, 200), RGBA { r: 0, g: 0, b: 255, a: 0 });
         canvas.draw_line(Vec2i(0, 0), Vec2i(200, 200), RGBA { r: 255, g: 255, b: 255, a: 0 });
         canvas.fill_triangle(Vec2i(0, 300), Vec2i(300, 300), Vec2i(300, 450), RGBA { r: 255, g: 0, b: 0, a: 0 });
@@ -59,7 +67,7 @@ fn main() {
         canvas.fill_polygon(vec!{Vec2i(0, 0), Vec2i(200, 200)}, RGBA { r: 255, g: 0, b: 255, a: 0 });
         canvas.fill_polygon(vec!{Vec2i(0, 300), Vec2i(300, 300), Vec2i(300, 450), Vec2i(450, 1000)}, RGBA { r: 255, g: 255, b: 0, a: 0 });
 
-        window.update_with_buffer(&canvas.buffer, WIDTH, HEIGHT).unwrap();
+        window.update_with_buffer(&canvas.buffer, canvas.width as usize, canvas.height as usize).unwrap();
     }
 
 
